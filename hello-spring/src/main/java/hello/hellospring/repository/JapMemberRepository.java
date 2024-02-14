87169874e5b2837904790c710aa5a7830c2349c3
soonggi <soonggi99@gmail.com>
2024-02-14 14:31:51 +0900

    first commit

---
+++ hello-spring/src/main/java/hello/hellospring/repository/JapMemberRepository.java
@@ -0,0 +1,39 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+import jakarta.persistence.EntityManager;
+
+import java.util.List;
+import java.util.Optional;
+
+public class JapMemberRepository implements MemberRepository{
+
+    private final EntityManager em;
+
+    public JapMemberRepository(EntityManager em) {
+        this.em = em;
+    }
+
+    public JapMemberRepository() {
+    }
+
+    @Override
+    public Member save(Member member) {
+        return null;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return null;
+    }
+}
